6c38f4a4d2bc7c6371b28ada59b5461058ead095
Thorsten Fischer <thorsten@froschi.org>
2012-06-07 16:56:42 +0200

    Add dependencies for libmagickcore2 on lucid.
    There are 14 of them.
    
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,24 @@
-packages = %w/
-  libmagickcore2
-/
+packages = Array.new
+
+case node[:lsb][:codename]
+when "lucid"
+  include_recipe "libbz2"
+  include_recipe "libfreetype"
+  include_recipe "libgomp"
+  include_recipe "libice"
+  include_recipe "libjasper"
+  include_recipe "libjpeg"
+  include_recipe "liblcms"
+  include_recipe "libltdl"
+  include_recipe "libpng"
+  include_recipe "libsm"
+  include_recipe "libtiff"
+  include_recipe "libxext"
+  include_recipe "libxml2"
+  include_recipe "libxt"
+
+  packages |= %w/libmagickcore2/
+end
 
 packages.each do |pkg|
   package pkg do
